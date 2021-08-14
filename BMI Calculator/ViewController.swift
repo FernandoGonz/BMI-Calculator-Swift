b97c053bcd619f4cb1dcee131f602e5b4491da13
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-08-14 11:51:19 -0500

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Fernando González on 14/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
